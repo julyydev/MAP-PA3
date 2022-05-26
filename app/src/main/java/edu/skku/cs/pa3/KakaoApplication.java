69416fc06c38609617a5e285e718f1f149d47529
Julyy <66120479+kwakjy@users.noreply.github.com>
2022-05-27 01:59:57 +0900

    Feat: Implement Kakao Login

---
+++ app/src/main/java/edu/skku/cs/pa3/KakaoApplication.java
@@ -0,0 +1,15 @@
+package edu.skku.cs.pa3;
+
+import android.app.Application;
+
+import com.kakao.sdk.common.KakaoSdk;
+
+public class KakaoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        KakaoSdk.init(this, "844d3dd264bd5cee029a1f798a2fabd7");
+    }
+}
